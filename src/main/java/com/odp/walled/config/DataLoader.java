f2fd9a12ffdbfc1a3e2a5b8170a21c35e9689351
Adham Muhammad <adhamfarid1110@gmail.com>
2025-03-28 23:46:36 +0700

    feat: create user also create user's wallet

--- src/main/java/com/odp/walled/config/DataLoader.java
+++ src/main/java/com/odp/walled/config/DataLoader.java
@@ -1,11 +1,10 @@
 package com.odp.walled.config;
 
-import com.odp.walled.model.User;
-import com.odp.walled.repository.UserRepository;
+import com.odp.walled.dto.UserRequest;
+import com.odp.walled.service.UserService;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -16,61 +15,88 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Component
 public class DataLoader implements ApplicationRunner {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    public DataLoader(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public DataLoader(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public void run(ApplicationArguments args) {
-        List<User> users = Arrays.asList(
-                User.builder()
+        // password123
+        List<UserRequest> userRequests = Arrays.asList(
+                UserRequest.builder()
                         .email("john.doe1@mail.com")
                         .username("johndoe1")
                         .fullname("John Doe")
-                        .password("password123")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
                         .phoneNumber("284184218421")
-                        .avatarUrl(null)
                         .build(),
-
-                User.builder()
+                UserRequest.builder()
                         .email("jane.smith2@mail.com")
                         .username("janesmith2")
                         .fullname("Jane Smith")
-                        .password("password123")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
                         .phoneNumber("284184218422")
-                        .avatarUrl(null)
                         .build(),
-
-                User.builder()
+                UserRequest.builder()
                         .email("michael.brown3@mail.com")
                         .username("michaelbrown3")
                         .fullname("Michael Brown")
-                        .password("password123")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
                         .phoneNumber("284184218423")
-                        .avatarUrl(null)
                         .build(),
-
-                User.builder()
+                UserRequest.builder()
                         .email("emily.davis4@mail.com")
                         .username("emilydavis4")
                         .fullname("Emily Davis")
-                        .password("password123")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
                         .phoneNumber("284184218424")
-                        .avatarUrl(null)
                         .build(),
-
-                User.builder()
+                UserRequest.builder()
                         .email("david.johnson5@mail.com")
                         .username("davidjohnson5")
                         .fullname("David Johnson")
-                        .password("password123")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
                         .phoneNumber("284184218425")
-                        .avatarUrl(null)
+                        .build(),
+                UserRequest.builder()
+                        .email("sarah.wilson6@mail.com")
+                        .username("sarahwilson6")
+                        .fullname("Sarah Wilson")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
+                        .phoneNumber("284184218426")
+                        .build(),
+                UserRequest.builder()
+                        .email("chris.lee7@mail.com")
+                        .username("chrislee7")
+                        .fullname("Chris Lee")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
+                        .phoneNumber("284184218427")
+                        .build(),
+                UserRequest.builder()
+                        .email("olivia.moore8@mail.com")
+                        .username("oliviamoore8")
+                        .fullname("Olivia Moore")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
+                        .phoneNumber("284184218428")
+                        .build(),
+                UserRequest.builder()
+                        .email("daniel.thomas9@mail.com")
+                        .username("danielthomas9")
+                        .fullname("Daniel Thomas")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
+                        .phoneNumber("284184218429")
+                        .build(),
+                UserRequest.builder()
+                        .email("emma.white10@mail.com")
+                        .username("emmawhite10")
+                        .fullname("Emma White")
+                        .password("$2a$10$VEbeUeYwJllVLMUL2Xfhcuxqi6twJRkpD9sadUCiOOexaXdeCh.Yu")
+                        .phoneNumber("284184218430")
                         .build());
 
-        userRepository.saveAll(users);
+        userRequests.forEach(userService::createUser);
     }
 }
