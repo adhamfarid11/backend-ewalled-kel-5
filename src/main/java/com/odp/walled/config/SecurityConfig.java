892c77b27907ae6d7f99c7c67c6bdc7e2f900194
Adham Muhammad <adhamfarid1110@gmail.com>
2025-04-13 21:51:12 +0700

    chore: change allowed origins in SecurityConfig.java

--- src/main/java/com/odp/walled/config/SecurityConfig.java
+++ src/main/java/com/odp/walled/config/SecurityConfig.java
@@ -68,7 +68,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://yourfrontend.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                "http://localhost:8081",
+                "http://10.0.2.2:8080",
+                "http://localhost:8082",
+                "http://127.0.0.1:8080"));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
